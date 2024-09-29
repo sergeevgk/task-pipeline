de7c791ffd49aa35260ff81c92ea6c8530d1820d
sergeevgk <sergeevgk.srb@gmail.com>
2024-09-30 00:39:03 +0300

    Add validation for the Task AverageTime as the external program execution time depends on that property. Default to 0.

--- TaskPipeline.ApiService/ExampleSchemaFilter.cs
+++ TaskPipeline.ApiService/ExampleSchemaFilter.cs
@@ -7,7 +7,7 @@ public class ExamplesSchemaFilter : ISchemaFilter
 {
 	public void Apply(OpenApiSchema schema, SchemaFilterContext context)
 	{
-		if (context.Type == typeof(Models.Task))
+		if (context.Type == typeof(Models.TaskDto))
 		{
 			schema.Example = new OpenApiObject
 			{
@@ -15,21 +15,17 @@ public class ExamplesSchemaFilter : ISchemaFilter
 				["name"] = new OpenApiString("Sample Task"),
 				["averageTime"] = new OpenApiDouble(0),
 				["description"] = new OpenApiString("This is a sample task description."),
-				["createdBy"] = new OpenApiString("Alice"),
 				["createdDate"] = new OpenApiDateTime(DateTime.UtcNow),
 				["shouldCompleteUnsuccessfully"] = new OpenApiBoolean(false)
 			};
 		}
 
-		if (context.Type == typeof(Models.Pipeline))
+		if (context.Type == typeof(Models.PipelineDto))
 		{
-			schema.Properties.Remove("totalTime");
-			schema.Properties.Remove("pipelineRunTime");
 			schema.Example = new OpenApiObject
 			{
 				["id"] = new OpenApiInteger(0),
 				["name"] = new OpenApiString("Sample Pipeline"),
-				["items"] = new OpenApiArray(),
 				["description"] = new OpenApiString("This is a sample pipeline description."),
 				["status"] = new OpenApiString("None")
 			};
--- TaskPipeline.ApiService/Models/TaskDto.cs
+++ TaskPipeline.ApiService/Models/TaskDto.cs
@@ -1,4 +1,6 @@
-﻿namespace TaskPipeline.ApiService.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TaskPipeline.ApiService.Models;
 
 public class TaskDto
 {
@@ -8,7 +10,8 @@ public class TaskDto
 	/// Estimated average run time of the task in a pipeline in seconds.
 	/// </summary>
 	/// <example>10.0</example>
-	public double AverageTime { get; set; }
+	[Range(0, 86400)]
+	public double AverageTime { get; set; } = 0;
 	public string? Description { get; set; }
 	public DateTime CreatedDate { get; set; }
 	public bool ShouldCompleteUnsuccessfully { get; set; } = false;
